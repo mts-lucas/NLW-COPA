b628f1686c94e21ad50faf1553b282474d4f71ad
Lucas Mateus <lmateus1067@outlook.com>
2022-11-03 09:27:17 -0300

    .

--- main.js
+++ main.js
@@ -32,19 +32,3 @@ document.querySelector("#app").innerHTML = `
     ${createCard("24/11", "Quinta", createGame("switzerland", "07:00", "cameroon") + createGame("south-korea", "10:00", "uruguay") + createGame("portugal", "13:00", "ghana") + createGame("brazil", "16:00", "serbia"))}
 </main>
 `
-
-{/* <li>
-    <img src="./assets/icon-south-korea.svg" alt="South Korea">
-    <strong>10:00</strong>
-    <img src="./assets/icon-uruguay.svg" alt="Uruguay">
-</li>
-<li>
-    <img src="./assets/icon-portugal.svg" alt="Portugal">
-    <strong>13:00</strong>
-    <img src="./assets/icon-ghana.svg" alt="Ghana">
-</li>
-<li>
-    <img src="./assets/icon_brazil.svg" alt="Brasil">
-    <strong>16:00</strong>
-    <img src="./assets/icon-serbia.svg" alt="Servia">
-</li> */}
